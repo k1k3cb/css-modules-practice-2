9a0371517fa954e8220e908b4c3032b045a1fbfe
k1k3cb <enriquecbar@gmail.com>
2024-01-03 14:20:19 +0100

    map optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
+import { v4 } from 'uuid';
 import Card from './components/card/card';
 import CardGrid from './components/cardGrid/CardGrid';
 import Header from './components/header/Header';
+import { cards } from './constants/cards';
 
 const App = () => {
 	return (
 		<div className='container'>
 			<Header />
 			<CardGrid>
-				<Card
-					cardStyle='card1'
-					title='Supervisor'
-					color='green'
-					text='Scans our talent network to create the optimal team for your project'
-					image='/images/icon-supervisor.svg'
-				/>
-				<Card
-					cardStyle='card2'
-					title='Team Builder'
-					color='red'
-					text='Scans our talent network to create the optimal team for your project'
-					image='/images/icon-team-builder.svg'
-				/>
-				<Card
-					cardStyle='card3'
-					title='Karma'
-					color='yellow'
-					text='Scans our talent network to create the optimal team for your project'
-					image='/images/icon-karma.svg'
-				/>
-				<Card
-					cardStyle='card4'
-					title='Calculator'
-					color='blue'
-					text='Scans our talent network to create the optimal team for your project'
-					image='/images/icon-calculator.svg'
-				/>
+				{cards.map(card => (
+					<Card
+						key={v4()}
+						cardStyle={card.cardStyle}
+						title={card.title}
+						color={card.color}
+						text={card.text}
+						image={card.image}
+					/>
+				))}
 			</CardGrid>
 		</div>
 	);
---
+++ src/constants/cards.js
@@ -0,0 +1,30 @@
+export const cards = [
+	{
+		cardStyle: 'card1',
+		title: 'Supervisor',
+		color: 'green',
+		text: 'Scans our talent network to create the optimal team for your project',
+		image: '/images/icon-supervisor.svg'
+	},
+	{
+		cardStyle: 'card2',
+		title: 'Team Builder',
+		color: 'red',
+		text: 'Scans our talent network to create the optimal team for your project',
+		image: '/images/icon-team-builder.svg'
+	},
+	{
+		cardStyle: 'card3',
+		title: 'Karma',
+		color: 'yellow',
+		text: 'Scans our talent network to create the optimal team for your project',
+		image: '/images/icon-karma.svg'
+	},
+	{
+		cardStyle: 'card4',
+		title: 'Calculator',
+		color: 'blue',
+		text: 'Scans our talent network to create the optimal team for your project',
+		image: '/images/icon-calculator.svg'
+	}
+];
